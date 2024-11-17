44ca3d27ec862ba01f23d78b8d1fecf374016013
ichastevani <ichastevani86@gmail.com>
2024-11-17 12:51:18 +0700

    update validation password

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -3,12 +3,12 @@ import { FaUser, FaEnvelope, FaPhone, FaLock } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import logoIcon from "../assets/logoicon.png";
 import signupImage from "../assets/signupImage.png";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const SignUp = () => {
   const navigate = useNavigate();
-// State untuk menyimpan data form
+  // State untuk menyimpan data form
   const [formData, setFormData] = useState({
     fullname: "",
     email: "",
@@ -25,8 +25,7 @@ const SignUp = () => {
     password: "",
     confirmPassword: "",
   });
-  
-  
+
   // State untuk modal sukses
   const [showModal, setShowModal] = useState(false);
   // Handle perubahan input
@@ -43,15 +42,15 @@ const SignUp = () => {
   // Fungsi untuk validasi password
   const isPasswordStrong = (password) => {
     const passwordRegex =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_])[A-Za-z\d@$!%*?&_]{8,}$/;
     return passwordRegex.test(password);
   };
 
-// Handle submit form
+  // Handle submit form
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-   // Validasi input
+    // Validasi input
     let errors = {};
     if (!formData.fullname) errors.fullname = "Full Name is required.";
     if (!formData.email) errors.email = "Email is required.";
@@ -71,7 +70,10 @@ const SignUp = () => {
 
     try {
       // Kirim data ke backend
-      const response = await axios.post("http://localhost:5000/api/users/signup", formData);
+      const response = await axios.post(
+        "http://localhost:5000/api/users/signup",
+        formData
+      );
 
       // Tampilkan modal sukses
       setShowModal(true);
@@ -81,18 +83,21 @@ const SignUp = () => {
       }, 3000);
     } catch (err) {
       // Tangani error dari backend
-      setFieldErrors({ email: err.response?.data?.error || "Something went wrong!" });
+      setFieldErrors({
+        email: err.response?.data?.error || "Something went wrong!",
+      });
     }
   };
 
-   return (
-
-
-    
+  return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 relative font-poppins">
       {/* Logo Section */}
       <div className="absolute top-[30px] left-[30px] flex items-center space-x-2">
-        <img src={logoIcon} alt="Aglostok logo" className="w-[69.06px] h-[69.06px]" />
+        <img
+          src={logoIcon}
+          alt="Aglostok logo"
+          className="w-[69.06px] h-[69.06px]"
+        />
         <h1 className="text-[35px] font-bold text-green-700">Aglostok</h1>
       </div>
 
@@ -100,10 +105,24 @@ const SignUp = () => {
         {/* Left Side - Form Section */}
         <div className="flex-1 px-8 py-16 flex flex-col justify-center ml-[132.65px] mt-[132.65px] max-w-[450px] space-y-6">
           <div className="flex justify-center items-center mb-8 text-gray-500 pb-10 space-x-10">
-            <NavLink to="/login" className={({ isActive }) => isActive ? "text-[40px] font-semibold text-green-700 leading-[36px] relative" : "text-[40px] font-semibold text-gray-400 leading-[36px]"}>
+            <NavLink
+              to="/login"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-[40px] font-semibold text-green-700 leading-[36px] relative"
+                  : "text-[40px] font-semibold text-gray-400 leading-[36px]"
+              }
+            >
               Login
             </NavLink>
-            <NavLink to="/signup" className={({ isActive }) => isActive ? "text-[40px] font-bold text-green-700 leading-[36px] relative" : "text-[40px] font-semibold text-gray-400 leading-[36px]"}>
+            <NavLink
+              to="/signup"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-[40px] font-bold text-green-700 leading-[36px] relative"
+                  : "text-[40px] font-semibold text-gray-400 leading-[36px]"
+              }
+            >
               Sign up
               {window.location.pathname === "/signup" && (
                 <span className="block h-[4px] w-[140px] bg-green-700 rounded-full mt-1 mx-auto"></span>
@@ -131,13 +150,15 @@ const SignUp = () => {
                 </div>
                 {/* Error Message */}
                 {fieldErrors.fullname && (
-                  <p className="text-red-500 text-sm ml-2 pl-2 w-[150px] text-left">{fieldErrors.fullname}</p>
+                  <p className="text-red-500 text-sm ml-2 pl-2 w-[150px] text-left">
+                    {fieldErrors.fullname}
+                  </p>
                 )}
               </div>
             </div>
 
-             {/* Email Input */}
-             <div className="relative flex flex-row items-center">
+            {/* Email Input */}
+            <div className="relative flex flex-row items-center">
               <div className="flex flex-col w-full">
                 <div className="flex items-center bg-white shadow-md rounded-full h-[62px] px-4 border border-gray-200">
                   <FaEnvelope className="text-[#457468] text-lg mr-4" />
@@ -152,7 +173,9 @@ const SignUp = () => {
                 </div>
                 {/* Error Message */}
                 {fieldErrors.email && (
-                  <p className="text-red-500 text-sm ml-2 pl-2 w-[150px] text-left">{fieldErrors.email}</p>
+                  <p className="text-red-500 text-sm ml-2 pl-2 w-[150px] text-left">
+                    {fieldErrors.email}
+                  </p>
                 )}
               </div>
             </div>
@@ -173,7 +196,9 @@ const SignUp = () => {
                 </div>
                 {/* Error Message */}
                 {fieldErrors.phone && (
-                  <p className="text-red-500 text-sm ml-2 pl-2 w-auto text-left whitespace-nowrap">{fieldErrors.phone}</p>
+                  <p className="text-red-500 text-sm ml-2 pl-2 w-auto text-left whitespace-nowrap">
+                    {fieldErrors.phone}
+                  </p>
                 )}
               </div>
             </div>
@@ -194,7 +219,9 @@ const SignUp = () => {
                 </div>
                 {/* Error Message */}
                 {fieldErrors.password && (
-                  <p className="text-red-500 text-sm ml-2 pl-2 w-[250px] text-left">{fieldErrors.password}</p>
+                  <p className="text-red-500 text-sm ml-2 pl-2 w-[250px] text-left">
+                    {fieldErrors.password}
+                  </p>
                 )}
               </div>
             </div>
@@ -215,18 +242,30 @@ const SignUp = () => {
                 </div>
                 {/* Error Message */}
                 {fieldErrors.confirmPassword && (
-                  <p className="text-red-500 text-sm ml-2 pl-2 w-auto text-left whitespace-nowrap">{fieldErrors.confirmPassword}</p>
+                  <p className="text-red-500 text-sm ml-2 pl-2 w-auto text-left whitespace-nowrap">
+                    {fieldErrors.confirmPassword}
+                  </p>
                 )}
               </div>
             </div>
 
             {/* Terms and Conditions Checkbox */}
             <div className="flex items-center space-x-2 text-gray-500">
-              <input type="checkbox" className="form-checkbox h-4 w-4 text-green-600 rounded" required />
+              <input
+                type="checkbox"
+                className="form-checkbox h-4 w-4 text-green-600 rounded"
+                required
+              />
               <p className="text-sm">
                 By signing up you agree to our{" "}
-                <span className="text-green-700 cursor-pointer">terms & conditions</span> of use and{" "}
-                <span className="text-green-700 cursor-pointer">privacy policy</span>.
+                <span className="text-green-700 cursor-pointer">
+                  terms & conditions
+                </span>{" "}
+                of use and{" "}
+                <span className="text-green-700 cursor-pointer">
+                  privacy policy
+                </span>
+                .
               </p>
             </div>
 
@@ -250,15 +289,20 @@ const SignUp = () => {
         </div>
       </div>
       {showModal && (
-      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-        <div className="bg-white p-6 rounded-lg shadow-lg text-center">
-          <h2 className="text-green-700 text-2xl font-semibold mb-4">Sign Up Successful!</h2>
-          <p className="text-gray-600">You have successfully created an account.</p>
-          <p className="text-sm text-gray-500 mt-4">Redirecting to login page...</p>
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+            <h2 className="text-green-700 text-2xl font-semibold mb-4">
+              Sign Up Successful!
+            </h2>
+            <p className="text-gray-600">
+              You have successfully created an account.
+            </p>
+            <p className="text-sm text-gray-500 mt-4">
+              Redirecting to login page...
+            </p>
+          </div>
         </div>
-      </div>
-    )}
-
+      )}
     </div>
   );
 };
